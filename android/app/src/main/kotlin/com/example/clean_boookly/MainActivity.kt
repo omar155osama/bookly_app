6161a5e65976eafd38975131b300063c3f0c4447
omar155osama <139631470+omar155osama@users.noreply.github.com>
2026-01-31 22:13:08 +0200

    Initial clean Flutter project

---
+++ android/app/src/main/kotlin/com/example/clean_boookly/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.clean_boookly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/features/splash/presention/views/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:clean_boookly/core/constant.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 1600),
+      vsync: this,
+    );
+    _fadeAnimation = Tween(begin: 0.0, end: 1.0).animate(_animationController);
+    _animationController.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: FadeTransition(
+        opacity: _fadeAnimation,
+        child: Center(
+          child: Image.asset("assets/Logo.png", fit: BoxFit.contain),
+        ),
+      ),
+    );
+  }
+}
